82afd6db6107dc334a168e3ed696c1056a091bc5
balaji1732000 <balajisampath777@gmail.com>
2024-01-03 14:42:41 +0530

    first commit

---
+++ src/components/config.js
@@ -0,0 +1,12 @@
+
+export const avatarAppConfig = {
+    cogSvcRegion : "eastus",
+    cogSvcSubKey : "40c9a21dddee429b86775c71351c57ad",
+    voiceName : "en-US-JennyNeural",
+    avatarCharacter : "lisa",
+    avatarStyle : "casual-sitting",
+    avatarBackgroundColor : "#FFFFFFFF",
+    iceUrl : "stun:relay.communication.microsoft.com:3478",
+    iceUsername : "BQAANvhgJQAB2ij1W9e4HMurB7+vGKLBRuz7smIAa5kAAAAMARC/dFx4T8BD+qPgHQdHSP+sd7aF9X278F7/9JQ+J1NZugMpoyg=",
+    iceCredential : "L5CQRC0x20LHpzwdmfHR1suj8GY="
+  }
